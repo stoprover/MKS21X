51f317ce72ea5b70e4413e7a3c58ef40c8d49001
stoprover <steventoprover@gmail.com>
2016-12-02 06:31:12 -0500

    por favor

---
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+public class SuperArrayIterator implements Iterator<String>{
+private SuperArray whichOne; //this may not be the right type, fix it later
+private String element;
+public SuperArrayIterator(SuperArray alpha){
+//in superarray, alpha will be replaced by this
+
+
+
+
+
+
+
+
+
+
+//again, idk what types the elements are, so fix it when necessary
+//constructor initil
+//instatniatize copy of iterator in superarray and thats it
+//return new iterator(this)?????????????????
+//public class SuperArray implements Iterable<String>{
+//public SuperArrayIterator( ){
+//return new SuperArrayIterator(this);
+
