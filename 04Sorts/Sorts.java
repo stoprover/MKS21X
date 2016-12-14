2c2d7499d920c1d7c38177ac841809b988f9992a
stoprover <steventoprover@gmail.com>
2016-12-14 06:38:10 -0500

    meh sorts

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -12,22 +12,40 @@ public class Sorts{
 		    indOfSSF = i;
 		}
 	    }
-	    for (int j = 0; j < data.length; j++){
+	    data[indOfSSF]= data[m];//may be wrong
+	    data[m] = smallestSoFar;//may be wrong
+	    /**for (int j = 0; j < data.length; j++){
 		if (data[j] > smallestSoFar){
 		    data[indOfSSF] = data[j];
 		    data[j] = smallestSoFar;
 		    j = data.length;
 		}
-	    }
+		}*/
 	}
     }
+    public static void insertionSort(int[]data){
+	int indWhereShould;//backwards inner loop
+	for (int i = 0; i < data.length - 1; i++){
+	    for (int j = i + 1; j < data.length; j++){
+		if (data[i] > data[j]){
+		    indWhereShould = j;
+		}
+	/**for (int elLookingAt = 1; elLookingAt < data.length - 1; elLookingAt++){
+	    for (int i = elLookingAt + 1; i < 	    
+if (data[elLookingAt] > data[elLookingAt + 1]){
+		moved = data[elLookingAt + 1];
+		data[elLookingAt + 1] = data[elLookingAt];
+		data[elLookingAt] = moved;
+	    }
+	}
+	}*/
     public static void main(String[]args){
 	int[]ary = new int[5];
 	ary[0] = 64;
 	ary[1] = 25;
 	ary[2] = 12;
 	ary[3] = 22;
-	ary[4] = 11;
+	ary[4] = 11;//64, 25, 12, 22, 11;     25, 64, 12,22,11
 	selectionSort(ary);
 	System.out.println(ary[0] + " " + ary[1] + " " + ary[2] + " " + ary[3] + " " + ary[4]);
 	int[]dairy = new int[4];
