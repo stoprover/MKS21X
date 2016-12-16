9feaf98338ba6c504eba61677d147b2c3e82d1fb
Galina Gorina <Galina Gorina>
2016-12-15 23:17:23 -0500

    working bubbleSort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -42,9 +42,23 @@ public class Sorts{
 	    data[placeToInsert] = smallestRNU;
 	}
     }
-    public static void bubbleSort(int[]data){//loop through to second-to-last,compare data[counter] and data[counter + 1], swap if necessary. also make sure to have a swap variable, always set it back to zero whenever you start from the beginning again, and if its > 0, go through again; else, stop the swapping. furthemore, there should be a way to go back from last digits, (make the test for the data get smaller and smaller if the last ones are well-sorted and don't need to be swapped)since 5 and 8 are gucci, don't look at it again, instead look at 2 and 5.
-	//system.currentTimeMillis(), don't measure sort if wrong; is a thing that you should use!!!!!!!!!!!!!!!!!!!!  
+    public static void bubbleSort(int[]data){
+	int numSwaps = 1;
+	int temp;
+	while (numSwaps != 0){
+	    numSwaps = 0;
+	    for (int i = 0; i < data.length - 1; i++){
+		if (data[i] > data[i + 1]){
+		    temp = data[i+1];
+		    data[i + 1] = data[i];
+		    data[i] = temp;
+		    numSwaps++;
+		}
+	    }
+	}
     }
+	//loop through to second-to-last,compare data[counter] and data[counter + 1], swap if necessary. also make sure to have a swap variable, always set it back to zero whenever you start from the beginning again, and if its > 0, go through again; else, stop the swapping. furthemore, there should be a way to go back from last digits, (make the test for the data get smaller and smaller if the last ones are well-sorted and don't need to be swapped)since 5 and 8 are gucci, don't look at it again, instead look at 2 and 5.
+	//system.currentTimeMillis(), don't measure sort if wrong; is a thing that you should use!!!!!!!!!!!!!!!!!!!!  
     public static String printArray (int[]data){
 	String sum = "[";
 	for (int i = 0; i < data.length; i++){
@@ -66,16 +80,18 @@ public class Sorts{
 	ary[3] = 22;
 	ary[4] = 11;//64, 25, 12, 22, 11;     25, 64, 12,22,11
 	//selectionSort(ary);
-	insertionSort(ary);
-	System.out.println(printArray(ary));
+	//insertionSort(ary);
+	bubbleSort(ary);
+	System.out.println("ary.." + printArray(ary));
 	int[]dairy = new int[4];
 	dairy[0] = 54;
 	dairy[1] = 1;
 	dairy[2] = 32;
 	dairy[3] = 15;
 	//selectionSort(dairy);
-	insertionSort(dairy);
-	System.out.println(printArray(dairy));
+	//insertionSort(dairy);
+	bubbleSort(dairy);
+	System.out.println("dairy" + printArray(dairy));
 	System.out.println(name());
     }
 }
